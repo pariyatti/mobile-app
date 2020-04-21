6b5dadf48a23fcd6a6a5d69780b0749cf65fa0d5
Varun Barad <varun@obvious.in>
2020-04-21 18:29:50 +0530

    Create individual card to display inspiration of the day

---
+++ lib/inspiration_card.dart
@@ -0,0 +1,130 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InspirationCard extends StatelessWidget {
+  final String text;
+  final String imageUrl;
+
+  InspirationCard({Key key, this.text, this.imageUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 12.0,
+            ),
+            child: Card(
+              elevation: 2.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  8.0,
+                ),
+              ),
+              child: ClipPath(
+                clipper: ShapeBorderClipper(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      8.0,
+                    ),
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 12.0,
+                      ),
+                      child: Text(
+                        'Inspiration of the day'.toUpperCase(),
+                        style: TextStyle(
+                          inherit: true,
+                          fontSize: 14.0,
+                          color: Color(0xff999999),
+                        ),
+                      ),
+                    ),
+                    CachedNetworkImage(
+                      placeholder: (context, url) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                      errorWidget: (context, url, error) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Icon(Icons.error),
+                        ),
+                      ),
+                      imageUrl: imageUrl,
+                      imageBuilder: (context, imageProvider) {
+                        return Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Expanded(
+                              child: Image(
+                                image: imageProvider,
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        text,
+                        style: TextStyle(
+                          inherit: true,
+                          fontSize: 20.0,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Color(0xffdcd3c0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Expanded(
+                            child: MaterialButton(
+                              padding: EdgeInsets.zero,
+                              child: Icon(
+                                Icons.bookmark,
+                                color: Color(0xff6d695f),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Expanded(
+                            child: MaterialButton(
+                              padding: EdgeInsets.zero,
+                              child: Icon(
+                                CupertinoIcons.share_solid,
+                                color: Color(0xff6d695f),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:patta/inspiration_card.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,48 +16,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+class MyHomePage extends StatelessWidget {
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff4efe7),
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Column(
+        children: <Widget>[
+          InspirationCard(
+            text:
+                '\"We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow that never leaves.\"',
+//                    imageUrl: 'http://139.59.41.132/uploads/card/image/393b47a8-b002-4ff5-95f6-dd6df497cf69/bluesky.jpeg',
+//            imageUrl: 'https://images.unsplash.com/photo-1587321819113-cc19ebc6e5e3',
+//                    imageUrl: 'https://i.picsum.photos/id/9/250/250.jpg',
+            imageUrl:
+                'https://images.fatherly.com/wp-content/uploads/2019/01/scooby.jpg?q=65&enable=upscale&w=600',
+          ),
+        ],
       ),
     );
   }
