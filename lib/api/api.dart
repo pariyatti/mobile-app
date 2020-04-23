b9634ae0123e7fe07c13245813fb164e10f0c8e5
Varun Barad <varun@obvious.in>
2020-04-23 09:46:35 +0530

    Change base-url for api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:patta/api/model/today.dart';
 import 'package:patta/data_model/inspiration_card.dart';
 
-const _BASE_URL = 'http://139.59.41.132';
+const _BASE_URL = 'http://kosa-sandbox.pariyatti.org';
 
 Future<List<InspirationCardModel>> fetchToday() async {
   const TODAY_URL = '$_BASE_URL/api/today.json';
