b2f4061e6fa4cc47416b0ac7e2ca150b7db6ac64
Varun Barad <varun@obvious.in>
2020-04-24 12:14:08 +0530

    Add function to convert from database-card to corresponding card-model

--- lib/local_database/moor_converters.dart
+++ lib/local_database/moor_converters.dart
@@ -1,7 +1,8 @@
 import 'package:patta/data_model/inspiration_card.dart';
 import 'package:patta/local_database/database.dart';
 
-DatabaseCard toDatabaseCard(InspirationCardModel cardModel, DateTime createdAt) {
+DatabaseCard toDatabaseCard(
+    InspirationCardModel cardModel, DateTime createdAt) {
   return DatabaseCard(
     id: cardModel.id,
     type: 'inspiration',
@@ -9,4 +10,26 @@ DatabaseCard toDatabaseCard(InspirationCardModel cardModel, DateTime createdAt) 
     imageUrl: cardModel.imageUrl,
     createdAt: createdAt,
   );
+}
+
+CardModel toCardModel(DatabaseCard databaseCard) {
+  CardModel card;
+  switch (databaseCard.type) {
+    case 'inspiration':
+      {
+        card = InspirationCardModel(
+          id: databaseCard.id,
+          text: databaseCard.textData,
+          imageUrl: databaseCard.imageUrl,
+        );
+        break;
+      }
+    default:
+      {
+        card = null;
+        break;
+      }
+  }
+
+  return card;
 }
