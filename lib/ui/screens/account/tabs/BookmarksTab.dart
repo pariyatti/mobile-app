81472d4b4c7fabe75e0bb455d03319be503eaade
Varun Barad <varun@obvious.in>
2020-04-29 14:40:47 +0530

    Extract widgets into build methods

--- lib/ui/screens/account/tabs/BookmarksTab.dart
+++ lib/ui/screens/account/tabs/BookmarksTab.dart
@@ -20,65 +20,77 @@ class BookmarksTab extends StatelessWidget {
         AsyncSnapshot<List<DatabaseCard>> snapshot,
       ) {
         if (snapshot.hasData) {
-          final cardWidgets = snapshot.data
-              .map((dbCard) => moor_converters.toCardModel(dbCard))
-              .where((card) => (card != null))
-              .map((card) {
-                if (card is StackedInspirationCardModel) {
-                  return StackedInspirationCard(card, database);
-                } else if (card is PaliWordOfTheDayCardModel) {
-                  return PaliWordOfTheDayCard(card, database);
-                } else {
-                  return null;
-                }
-              })
-              .where((widget) => (widget != null))
-              .toList();
-
-          if (cardWidgets.isEmpty) {
-            return Center(
-              child: Text(
-                strings['en'].messageNothingBookmarked,
-                style: TextStyle(
-                  inherit: true,
-                  color: Color(0xff6d695f),
-                  fontSize: 16.0,
-                ),
-              ),
-            );
-          } else {
-            return ListView(children: cardWidgets);
-          }
+          return _buildCardsList(snapshot.data, database);
         } else if (snapshot.hasError) {
-          return Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.error,
-                    color: Color(0xff6d695f),
-                  ),
-                ),
-                Text(
-                  strings['en'].errorMessageTryAgainLater,
-                  style: TextStyle(
-                    inherit: true,
-                    color: Color(0xff6d695f),
-                    fontSize: 16.0,
-                  ),
-                )
-              ],
-            ),
-          );
+          return _buildError();
         } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
+          return _buildLoadingIndicator();
         }
       },
     );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildError() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.error,
+              color: Color(0xff6d695f),
+            ),
+          ),
+          Text(
+            strings['en'].errorMessageTryAgainLater,
+            style: TextStyle(
+              inherit: true,
+              color: Color(0xff6d695f),
+              fontSize: 16.0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCardsList(List<DatabaseCard> data, PariyattiDatabase database) {
+    final cardWidgets = data
+        .map((dbCard) => moor_converters.toCardModel(dbCard))
+        .where((card) => (card != null))
+        .map((card) {
+          if (card is StackedInspirationCardModel) {
+            return StackedInspirationCard(card, database);
+          } else if (card is PaliWordOfTheDayCardModel) {
+            return PaliWordOfTheDayCard(card, database);
+          } else {
+            return null;
+          }
+        })
+        .where((widget) => (widget != null))
+        .toList();
+
+    if (cardWidgets.isEmpty) {
+      return Center(
+        child: Text(
+          strings['en'].messageNothingBookmarked,
+          style: TextStyle(
+            inherit: true,
+            color: Color(0xff6d695f),
+            fontSize: 16.0,
+          ),
+        ),
+      );
+    } else {
+      return ListView(children: cardWidgets);
+    }
   }
 }
