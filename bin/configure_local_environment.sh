f8025975dff6474be90ae48a1bde1cf733ee42d5
Steven Deobald <steven@deobald.ca>
2022-01-15 16:12:44 -0600

    Small tweak to automatic IP detection
    * for some reason plugging in an iPhone via USB causes double IPs on a Mac? Weird.

--- bin/configure_local_environment.sh
+++ bin/configure_local_environment.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-LOCAL_IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+LOCAL_IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' \
+  | grep -Eo '([0-9]*\.){3}[0-9]*' \
+  | grep -v '127.0.0.1' \
+  | grep "192.168")" # sometimes with an iphone plugged in you'll get an extra IP like '169.254.56.99'
 printf "local IP is '%s'\n" "$LOCAL_IP"
 
 printf "setting local IP in lib/Environment.dart...\n"
-sed "s/\%MAKE_IP%/$LOCAL_IP/g" './lib/EnvironmentTemplate.dart' > './lib/Environment.dart'
+sed -e "s/%MAKE_IP%/$LOCAL_IP/g" './lib/EnvironmentTemplate.dart' > './lib/Environment.dart'
