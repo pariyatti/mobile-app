9d7422eabd080d2bcf22181f52a4f8b4863bd5c6
Steven Deobald <steven@deobald.ca>
2022-01-22 19:21:16 -0600

    Correcting `make run env=local` on MacOS/iOS
    * for some reason, my iPhone couldn't find my MacOS IP address, even though it could last week. The local environment config script now writes out the hostname for MacOS environments ... obviously this is an oversimplification, since this won't work on Android, but we can deal with it for now.

--- bin/configure_local_environment.sh
+++ bin/configure_local_environment.sh
@@ -6,5 +6,14 @@ LOCAL_IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' \
   | grep "192.168")" # sometimes with an iphone plugged in you'll get an extra IP like '169.254.56.99'
 printf "local IP is '%s'\n" "$LOCAL_IP"
 
-printf "setting local IP in lib/Environment.dart...\n"
-sed -e "s/%MAKE_IP%/$LOCAL_IP/g" './lib/EnvironmentTemplate.dart' > './lib/Environment.dart'
+LOCAL_HOST=$(hostname)
+printf "local hostname is '%s'\n" "$LOCAL_HOST"
+
+# TODO: this should check for an attached iPhone in addition to the host OS
+if [ "$(uname)" == "Darwin" ]; then
+  printf "MacOS found: setting local hostname in lib/Environment.dart...\n"
+  sed -e "s/%MAKE_IP%/$LOCAL_HOST/g" './lib/EnvironmentTemplate.dart' > './lib/Environment.dart'
+else
+  printf "Linux found: setting local IP in lib/Environment.dart...\n"
+  sed -e "s/%MAKE_IP%/$LOCAL_IP/g" './lib/EnvironmentTemplate.dart' > './lib/Environment.dart'
+fi
