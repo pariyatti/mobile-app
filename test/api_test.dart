266af5fcf9209ea8d716f4aeaf7bac6e90bfe77f
Steven Deobald <steven@deobald.ca>
2022-07-17 17:01:04 -0300

    Fixing test suite

--- test/api_test.dart
+++ test/api_test.dart
@@ -10,7 +10,7 @@ import 'package:patta/ui/model/StackedInspirationCardModel.dart';
 void main() {
   test('Test whether sample today api is parsed correctly or not', () {
     // setup
-    const baseUrl = '<giving-any-string-here-does-not-matter-for-this-test>';
+    const baseUrl = 'https://localhost:3000';
 
     //workaround for https://github.com/flutter/flutter/issues/20907
     String getContents(String filePath) {
@@ -33,7 +33,7 @@ void main() {
       cardModels[0],
       PaliWordCardModel(
         id: '98955ba9-3819-4662-bd7c-6bff3d035d6e',
-        url: '$baseUrl/api/v1/pali-word/98955ba9-3819-4662-bd7c-6bff3d035d6e',
+        url: '$baseUrl/api/v1/today/pali-words/98955ba9-3819-4662-bd7c-6bff3d035d6e.json',
         isBookmarkable: true,
         isShareable: true,
         header: 'Pāli Word of the Day',
@@ -48,7 +48,7 @@ void main() {
       cardModels[1],
       StackedInspirationCardModel(
         id: 'b4285e6a-ea3c-4fc1-a7f4-4de3f1ed8ca4',
-        url: '$baseUrl/api/v1/stacked-inspiration/b4285e6a-ea3c-4fc1-a7f4-4de3f1ed8ca4',
+        url: '$baseUrl/api/v1/today/stacked-inspirations/b4285e6a-ea3c-4fc1-a7f4-4de3f1ed8ca4.json',
         isBookmarkable: true,
         isShareable: true,
         header: 'Inspiration of the Day',
@@ -63,7 +63,7 @@ void main() {
       cardModels[2],
       OverlayInspirationCardModel(
         id: 'f9a2cc0f-915f-4a9a-a049-77bbb9be1fc7',
-        url: '$baseUrl/api/v1/overlay-inspiration/f9a2cc0f-915f-4a9a-a049-77bbb9be1fc7',
+        url: '$baseUrl/api/v1/today/overlay-inspirations/f9a2cc0f-915f-4a9a-a049-77bbb9be1fc7.json',
         isBookmarkable: true,
         isShareable: true,
         header: 'Inspiration of the Day',
