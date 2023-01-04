f0276a1cdbb78cd14dbbebaf1543207a709977ce
Steven Deobald <steven@deobald.ca>
2023-01-04 14:36:44 -0600

    All "SNG-supported" URLs are now hard-coded

--- lib/model/Chanting.dart
+++ lib/model/Chanting.dart
@@ -7,10 +7,26 @@ class Chanting {
   Uri get specialUrl => _sngUrl;
   bool get isSpecialVisible => _isSngChanting;
 
-  final List<Uri> sngSupported = [Uri.parse("https://download.pariyatti.org/dwob/sutta_nipata_2_271.mp3"),
+  final List<Uri> sngSupported =
+  [ Uri.parse("https://download.pariyatti.org/dwob/dhammapada_1_1_1_2.mp3"),
+    Uri.parse("https://download.pariyatti.org/dwob/dhammapada_11_153_11_154.mp3"),
+    Uri.parse("https://download.pariyatti.org/dwob/dhammapada_11_154.mp3"),
     Uri.parse("https://download.pariyatti.org/dwob/digha_nikaya_2_221.mp3"),
+    Uri.parse("https://download.pariyatti.org/dwob/sutta_nipata_1_149.mp3"),
+    Uri.parse("https://download.pariyatti.org/dwob/sutta_nipata_1_150.mp3"),
     Uri.parse("https://download.pariyatti.org/dwob/sutta_nipata_2_238.mp3"),
-    Uri.parse("https://download.pariyatti.org/dwob/dhammapada_11_153_11_154.mp3")];
+    Uri.parse("https://download.pariyatti.org/dwob/sutta_nipata_2_271.mp3"),
+    ];
+
+  // consider loading these dynamically in the constructor? or from a file?
+  // dhammapada_1_1_1_2.mp3
+  // dhammapada_11_153_11_154.mp3
+  // dhammapada_11_154.mp3
+  // digha_nikaya_2_221.mp3
+  // sutta_nipata_1_149.mp3
+  // sutta_nipata_1_150.mp3
+  // sutta_nipata_2_238.mp3
+  // sutta_nipata_2_271.mp3
 
   Chanting(audioUrl, [isSngChanting = false]) {
     _audioUrl = audioUrl;
