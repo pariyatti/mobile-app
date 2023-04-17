913277e5597a29422bbb348ffcd1b8936421986a
Steven Deobald <steven@deobald.ca>
2023-04-17 02:06:15 -0300

    Extract i18n keys: feed names

--- lib/model/Feed.dart
+++ lib/model/Feed.dart
@@ -6,9 +6,4 @@ class Feed {
 
   get getKey => key;
 
-  static const List<Feed> all = <Feed>[pali, buddha, dohas, inspiration];
-  static const pali = Feed("pali", "Pāli Word");
-  static const buddha = Feed("buddha", "Words of the Buddha");
-  static const dohas = Feed("dohas", "Dhamma Verse");
-  static const inspiration = Feed("inspiration", "Inspiration");
 }
---
+++ lib/model/FeedList.dart
@@ -0,0 +1,21 @@
+import 'package:patta/app/I18n.dart';
+
+import 'Feed.dart';
+
+class FeedList {
+  var pali;
+  var buddha;
+  var dohas;
+  var inspiration;
+
+  FeedList() {
+    pali = Feed("pali", I18n.get().paliWord);
+    buddha = Feed("buddha", I18n.get().wordsOfTheBuddha);
+    dohas = Feed("dohas", I18n.get().dhammaVerse);
+    inspiration = Feed("inspiration", I18n.get().inspiration);
+  }
+
+  all() {
+    return <Feed>[pali, buddha, dohas, inspiration];
+  }
+}
